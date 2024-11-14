7479ec75e9fed4ea59e2ff4269485043a4037118
GerryCrooked <pinnhead1987@googlemail.com>
2024-11-14 13:57:51 +0100

    Update retrain.sh

--- webserver/app/retrain.sh
+++ webserver/app/retrain.sh
@@ -3,13 +3,10 @@
 # Pfade anpassen
 DATASET_OPEN="/dataset/open"
 DATASET_CLOSED="/dataset/closed"
-MODEL_PATH="/app/model/garage_door_model.keras"
+MODEL_PATH="/app/model/garage_door_model.h5"
 
 # Retrain das Modell
 python /app/retrain.py $DATASET_OPEN $DATASET_CLOSED $MODEL_PATH
-
-# Neustart des Image Analyzer Containers
-docker-compose restart image_analyzer
 
 # Leere den Dataset-Ordner
 rm -rf $DATASET_OPEN/*
