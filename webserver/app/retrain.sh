0f0cdef3fee58cb997d7705f1de899456f7d9f3b
GerryCrooked <pinnhead1987@googlemail.com>
2024-11-14 16:54:29 +0100

    Update retrain.sh

--- webserver/app/retrain.sh
+++ webserver/app/retrain.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Pfade anpassen
-DATASET_OPEN="/dataset/open"
-DATASET_CLOSED="/dataset/closed"
 MODEL_PATH="/app/model/garage_door_model.h5"
 
 # Retrain das Modell
-python /app/retrain.py $DATASET_OPEN $DATASET_CLOSED $MODEL_PATH
+python /app/retrain.py $MODEL_PATH
 
-# Leere den Dataset-Ordner
-rm -rf $DATASET_OPEN/*
-rm -rf $DATASET_CLOSED/*
+# Optional: Wenn du die Datensätze nach dem Training leeren möchtest, entferne das Kommentarzeichen
+# rm -rf /dataset/open/*
+# rm -rf /dataset/closed/*
